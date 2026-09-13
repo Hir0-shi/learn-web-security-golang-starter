a5cd0aabecd390cebf2967c1ffbf544ae8a1510d
Hir0-shi <62444653+Hir0-shi@users.noreply.github.com>
2026-09-13 06:05:52 +0200

    owasp top 10

--- internal/httpserver/app.go
+++ internal/httpserver/app.go
@@ -235,6 +235,7 @@ func New(database *sql.DB, logger *logging.Logger, options Options) (*Applicatio
 
 	handler := applyMiddleware(
 		mainMux,
+		nosniff,
 		cspNonce,
 		recoverPanics(logger, renderer),
 	)
